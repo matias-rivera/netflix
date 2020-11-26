8189955223baadb162b772d87b59b41b219912be
Matías Rivera <juanmatiasrivera@outlook.com>
2020-11-26 15:26:04 -0300

    'Try it Now' Input

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -1,5 +1,5 @@
 import React, { createContext, useContext,  useState } from 'react'
-import { Container, Frame, Title, Item, Inner, Header, Body} from './styles/accordion'
+import { Container, Title, Item, Inner, Header, Body} from './styles/accordion'
 
 const ToggleContext = createContext()
 
@@ -15,9 +15,6 @@ Accordion.Title = function AccordionTitle({ children, ...restProps}){
     return <Title {...restProps}>{children}</Title>
 }
 
-Accordion.Frame = function AccordionFrame({ children, ...restProps}){
-    return <Frame {...restProps}>{children}</Frame>
-}
 
 Accordion.Item = function AccordionItem({ children, ...restProps}){
     const [toggleShow, setToggleShow] = useState(false)
